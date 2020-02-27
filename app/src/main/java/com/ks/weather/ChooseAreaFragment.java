55e47cb5fc027da682286b766074e8b18da300ed
Heber <1591994251@qq.com>
2020-02-27 17:57:35 +0800

    1

--- app/src/main/java/com/ks/weather/ChooseAreaFragment.java
+++ app/src/main/java/com/ks/weather/ChooseAreaFragment.java
@@ -1,108 +1,281 @@
 package com.ks.weather;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringDef;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ks.weather.db.City;
+import com.ks.weather.db.County;
+import com.ks.weather.db.Province;
+import com.ks.weather.util.HttpUtil;
+import com.ks.weather.util.Utility;
+
+import org.json.JSONException;
+import org.litepal.crud.DataSupport;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ChooseAreaFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ChooseAreaFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ChooseAreaFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "ChooseAreaFragment";
+    private static final int LEVEL_PROVINCE = 0;
+    public static final int LEVEL_CITY = 1;
+    public static final int LEVEL_COUNTY = 2;
+    private ProgressDialog progressDialog;
+    private TextView titleText;
+    private Button backButton;
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private List<String> dataList = new ArrayList<>();
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    //省列表
+    private List<Province> provinceList;
 
-    public ChooseAreaFragment() {
-        // Required empty public constructor
-    }
+    //市列表
+    private List<City> cityList;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ChooseAreaFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ChooseAreaFragment newInstance(String param1, String param2) {
-        ChooseAreaFragment fragment = new ChooseAreaFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    //县列表
+    private  List<County> countyList;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    //选中的省份
+    private Province selectedProvince;
+
+    //选中的城市
+    private City selectedCity;
+
+    //当前选中的级别
+    private int currentLevel;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.choose_area, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+        View view = inflater.inflate(R.layout.choose_area,container,false);
+        titleText = (TextView) view.findViewById(R.id.title_text);
+        backButton = view.findViewById(R.id.back_button);
+        listView = view.findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        return view;
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               if(currentLevel == LEVEL_PROVINCE){
+                   selectedProvince = provinceList.get(position);
+                   queryCities();
+               }else if(currentLevel == LEVEL_CITY){
+                   selectedCity = cityList.get(position);
+                   queryCounties();
+               }else if(currentLevel == LEVEL_COUNTY){
+                   String weatherId= countyList.get(position).getWeatherId();
+                   if(getActivity() instanceof  MainActivity){
+                       Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                       intent.putExtra("weather_id",weatherId);
+                       startActivity(intent);
+                       getActivity().finish();
+                   }else  if(getActivity() instanceof WeatherActivity){
+                       WeatherActivity activity = (WeatherActivity) getActivity();
+                       activity.drawerLayout.closeDrawers();
+                       activity.swipeRefresh.setRefreshing(true);
+                       activity.requestWeather(weatherId);
+                   }
+               }
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(currentLevel == LEVEL_COUNTY){
+                    queryCities();
+                }else if(currentLevel == LEVEL_CITY){
+                    queryProvinces();
+                }
+            }
+        });
+        queryProvinces();
+    }
+
+    //查询全国所有的省份优先从数据库开始查起，如果没有再去服务器上面查询
+
+    private void queryProvinces(){
+        titleText.setText("中国");
+        backButton.setVisibility(View.GONE);
+        provinceList = DataSupport.findAll(Province.class);
+        if(provinceList.size()>0){
+            dataList.clear();
+            for(Province province : provinceList){
+                dataList.add(province.getProvinceName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_PROVINCE;
+        }else{
+            String address = "http://guolin.tech/api/china";
+            queryFromServer(address,"province");
+        }
+
+    }
+
+
+    //查询所有的市，优先从数据库查询，如果没有就从服务器上面查询
+
+    private  void queryCities(){
+        titleText.setText(selectedProvince.getProvinceName());
+        backButton.setVisibility(View.VISIBLE);
+        cityList = DataSupport.where("", String.valueOf(selectedProvince.getId())).find(City.class);
+        if(cityList.size()>0){
+            dataList.clear();
+            for(City city: cityList){
+                dataList.add(city.getCityName());
+            }
+            adapter.notifyDataSetChanged();
+            listView.setSelection(0);
+            currentLevel = LEVEL_CITY;
+        }
+        else{
+            int provinceCode = selectedProvince.getProvinceCode();
+            String address = "http://guolin.tech/api/china/"+provinceCode;
+            queryFromServer(address,"city");
+        }
+
+    }
+
+/**
+ * 查询选中市内所有的县，优先从数据库查询，如果没有查询到再去服务器上查询。
+ */
+        public void queryCounties(){
+            titleText.setText(selectedCity.getCityName());
+            backButton.setVisibility(View.VISIBLE);
+            countyList = DataSupport.where("",String.valueOf(selectedCity.getId())).find(County.class);
+            if(countyList.size()>0){
+                dataList.clear();
+                for(County county: countyList){
+                    dataList.add(county.getCountyName());
+                }
+                adapter.notifyDataSetChanged();
+                listView.setSelection(0);
+                currentLevel = LEVEL_COUNTY;
+            }
+            else {
+                int provinceCode = selectedProvince.getProvinceCode();
+                int cityCode = selectedCity.getCityCode();
+                String address = "http://guolin.tech/api/china/"+ provinceCode+"/"+cityCode;
+                queryFromServer(address,"county");
+            }
+        }
+
+
+    /**
+     * 根据传入的地址和类型从服务器上查询省市县数据。
+     */
+
+    private void queryFromServer(String address, final String type){
+        showProgressDialog();
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closeProgressDialog();
+                        Toast.makeText(getContext(),"",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText = response.body().string();
+                boolean result = false;
+                if("province".equals(type)){
+                    try {
+                        result = Utility.handleProvinceResponse(responseText);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else if("city".equals(type)){
+                    result = Utility.handleCityResponse(responseText,selectedProvince.getId());
+                }
+                else if("county".equals(type)){
+                    result = Utility.handleCityResponse(responseText,selectedCity.getId());
+                }
+
+                if(result){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeProgressDialog();
+                            if("".equals(type)){
+                                queryProvinces();
+                            }
+                            else if("city".equals(type)){
+                                queryCities();
+                            }
+                            else if("county".equals(type)){
+                                queryCounties();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 
     /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
+     * 显示进度对话框
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    private void showProgressDialog(){
+        if(progressDialog == null){
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage("正在加载...");
+            progressDialog.setCanceledOnTouchOutside(false);
+        }
+        progressDialog.show();
     }
+
+    /**
+     * 关闭进度对话框
+     */
+    private void closeProgressDialog(){
+        if(progressDialog != null){
+            progressDialog.dismiss();
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/ks/weather/WeatherActivity.java
+++ app/src/main/java/com/ks/weather/WeatherActivity.java
@@ -1,9 +1,25 @@
 package com.ks.weather;
 
+import android.support.v4.view.ScrollingView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    public SwipeRefreshLayout swipeRefresh;
+    private ScrollView weatherLayout;
+    private Button navButton;
+    private TextView titleCity;
+    private TextView titleUpdateTime;
+    private TextView degreeText;
+    private TextView weatherInfoText;
+    private LinearLayout forecastLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
